98e5b49d7927e6445d918bf12717196192e2be1f
jesus <jantolin@inhub.es>
2024-11-11 20:23:46 +0100

    Update configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jaf333.github.io/', // Añade esta línea
+  base: '/'  // Para página de usuario
 })
